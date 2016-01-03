cc16f6ace945059f70f5219795fc9cd8597752c5
Jason Mark <jmark@dsquare.io>
2016-01-03 05:07:51 -0800

    Added an iOS app.

---
+++ direct_overlay_ios/Direct Overlay/Direct Overlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Direct Overlay
+//
+//  Created by Jason Mark on 1/3/16.
+//  Copyright © 2016 TheDevTeam, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ direct_overlay_ios/Direct Overlay/Direct Overlay/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Direct Overlay
+//
+//  Created by Jason Mark on 1/3/16.
+//  Copyright © 2016 TheDevTeam, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *imgWatchOnPhone;
+@property (strong, nonatomic) IBOutlet UIImageView *imgBrowseOnTV;
+@property (strong, nonatomic) IBOutlet UIImageView *imgSlidingDrawer;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
