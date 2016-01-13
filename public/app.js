c5e0e022fa6ee926114d51783d4dfcf7922e49cc
Ver Starr <ver.starr@utexas.edu>
2016-01-12 18:41:48 -0600

    This is a running version of the Twitch Overlay app developed at the AT&T Developer Summit 2016 in Las Vegas, NV. Reached the Top 20

--- public/app.js
+++ public/app.js
@@ -31,12 +31,8 @@ $( document ).ready(function() {
 
 			  $.get("https://api.twitch.tv/kraken/streams?game=League+of+Legends", function(data){
 			  		console.log(data.streams[2].channel.name)
-			  		//var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
-			  		//console.log(twitchURL)
-			  		document.getElementById('twitch-live-channel').src = "http://player.twitch.tv/?channel=" + data.streams[2].channel.name;
-			  	//	document.getElementById('videoSample').src = "http://player.twitch.tv/" + data.streams[2].channel.name + "/hls";
-			  		//document.getElementById('videoSample').src = "http://player.twitch.tv/?channel=" + data.streams[2].channel.name + "&html5";
-			  		//http://player.twitch.tv/?channel=lirik&html5
+			  		var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  	document.getElementById('twitch-live_channel').src = twitchURL;
 			  });
 	  	});
 
@@ -71,45 +67,43 @@ $( document ).ready(function() {
 		        	window.location.replace("messenger.html");
 		        	break;
 		        case 51 : // 3
-		        	window.location.replace("weather.html");
+		        	window.location.replace("newsfeed.html");
 		        	break;
 		        case 52 : // 4
-		        	window.location.replace("stock.html");
-		        	break;
-		        case 53 : // 5
 		        	window.location.replace("sports.html");
 		        	break;
+		        case 53 : // 5
 		        case 54 : // 6
-		        	//$.get("https://api.twitch.tv/kraken/streams?game=League+of+Legends", function(data){
+		        	$.get("https://api.twitch.tv/kraken/streams?game=League+of+Legends", function(data){
 				  		console.log(data.streams[2].channel.name)
-				  		//var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
-				  		//console.log(twitchURL)
-				  	//	document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
-				  	//});
-				  	//break;
+				  		var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  		console.log(twitchURL)
+				  		document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  	});
+				  	break;
 		        case 55 : // 7
-		        	//$.get("https://api.twitch.tv/kraken/streams?game=Dota+2", function(data){
+		        	$.get("https://api.twitch.tv/kraken/streams?game=Dota+2", function(data){
 				  		console.log(data.streams[2].channel.name)
 				  		//var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
 				  		//console.log(twitchURL)
-				  	//	document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
-				  	//});
+				  		document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  	});
 				  	break;
 		        case 56 : // 8
-		        	//$.get("https://api.twitch.tv/kraken/streams?game=Hearthstone%3A%20Heroes%20of%20Warcraft", function(data){
+		        	$.get("https://api.twitch.tv/kraken/streams?game=Hearthstone%3A%20Heroes%20of%20Warcraft", function(data){
 				  		console.log(data.streams[2].channel.name)
 				  		//var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
 				  		//console.log(twitchURL)
-				  	//	document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
-				  	//});
+				  		document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  	});
 				  	break;
 		        case 57 : // 9
-		        	//$.get("https://api.twitch.tv/kraken/streams?game=Counter-Strike%3A%20Global%20Offensive", function(data){
+		        	$.get("https://api.twitch.tv/kraken/streams?game=Counter-Strike%3A%20Global%20Offensive", function(data){
 				  		console.log(data.streams[2].channel.name)
-				  		//var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  		var twitchURL = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
 				  		//console.log(twitchURL)
-				  	//	document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
-				  	//});
+				  		document.getElementById('twitch-live_channel').src = "http://www.twitch.tv/" + data.streams[2].channel.name + "/embed";
+				  	});
 				  	break;
 		        case 79 : // Info
 		        case 87 : // Rewind Trick play
